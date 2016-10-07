419dae006b5edd080065b49b10cd3380c5749b49
Andrea Crotti <andrea.crotti.0@gmail.com>
2016-10-07 11:28:04 +0100

    script to do the whole process

--- single_app/full_test.sh
+++ single_app/full_test.sh
@@ -14,3 +14,17 @@ echo "Clean db again"
 
 rm $DEFAULT_DB
 ./manage.py migrate_raw
+
+# TODO: run a normal migrate after this and check that the result is
+# exactly the same in both cases
+sqlite3 $DEFAULT_DB .dump > generated.sql
+
+rm $DEFAULT_DB
+./manage.py migrate
+
+sqlite3 $DEFAULT_DB .dump > official.sql
+
+echo "Generating diff between two schemas"
+
+# TODO: can I sort somewhow in a way that makes it easy to compare what is going on?
+diff generated.sql official.sql > schema_differences.diff
